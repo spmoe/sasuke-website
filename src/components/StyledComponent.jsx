b6c1f9b0228359f220d33eba5e2b22845e847cee
sasuke61-sol <dunomoe@hotmail.com>
2024-04-12 03:16:55 +0900

    build: create new app

--- src/components/StyledComponent.jsx
+++
@@ -1,5 +0,0 @@
-export const SectionTitle = ({ children }) => {
-  return (
-    <h1 className="text-[24px] md:text-[32px] text-center font-[800]  text-gray-800 dark:text-gray-200 uppercase">{children}</h1>
-  )
-}
